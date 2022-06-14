af278f84607c5fda83d8f9eea1e058a2d7f9973f
Brooke Rahn <brooke.rahn@gmail.com>
2022-06-13 19:09:32 -0700

    update index.js file

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ var cors = require("cors");
 
 app.use(express.static("public"));
 app.use(cors());
+
+app.get("/account/create/:name/:email/:password", function (req, res) {
+  res.send({
+    name: req.params.name,
+    email: req.params.email,
+    password: req.params.password,
+  });
+});
+
+app.get("/account/login/:email/:password", function (req, res) {
+  res.send({
+    email: req.params.string,
+    password: req.params.password,
+  });
+});
+
+app.get("/account/all", function (req, res) {
+  res.send({
+    name: "peter",
+    email: "peter@mit.edu",
+    password: "secret",
+  });
+});
+
+var port = 3000;
+app.listen(port);
+console.log("Running on port: " + port);
