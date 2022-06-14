4089a082bdfd615299e99a43c41c53e45994ff01
Brooke Rahn <brooke.rahn@gmail.com>
2022-06-13 17:12:42 -0700

    add all files from previous project

---
+++ public/home.js
@@ -0,0 +1,22 @@
+function Home() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Card
+          bgcolor="info"
+          txtcolor="black"
+          header="Bad Bank"
+          title="Welcome to Bad Bank"
+          text="Please create an account and then login in to make transactions. Your information may be shared by us to anyone at anytime. 😏"
+          body={
+            <img src="bank.png" className="img-fluid" alt="Responsive image" />
+          }
+        />
+      </div>
+    );
+  }
