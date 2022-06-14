206e48524c795adf66e5d907705d734123a97f51
Brooke Rahn <brooke.rahn@gmail.com>
2022-06-13 17:05:35 -0700

    Add alldata.js and bank.css files

---
+++ public/alldata.js
@@ -0,0 +1,26 @@
+function AllData() {
+    const ctx = React.useContext(UserContext);
+    return (
+      <>
+        {ctx.users.map((ctx, i) => (
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Card
+              key={i}
+              bgcolor="info"
+              txtcolor="gray"
+              header={"Account information for: " + ctx.name}
+              title={"Balance: $" + ctx.balance}
+              text={"Email: " + ctx.email}
+              body={"Password: " + ctx.password}
+            />
+          </div>
+        ))}
+      </>
+    );
+  }
