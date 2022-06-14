2a460d23630523174b1ea84fbb19f1edce8abda3
Brooke Rahn <brooke.rahn@gmail.com>
2022-06-13 21:22:09 -0700

    update index.js file

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 var express = require("express");
 var app = express();
 var cors = require("cors");
+var dal = require("./dal.js");
 
 app.use(express.static("public"));
 app.use(cors());
 
 app.get("/account/create/:name/:email/:password", function (req, res) {
-  res.send({
-    name: req.params.name,
-    email: req.params.email,
-    password: req.params.password,
-  });
-});
-
-app.get("/account/login/:email/:password", function (req, res) {
-  res.send({
-    email: req.params.string,
-    password: req.params.password,
-  });
+  dal
+    .create(req.params.name, req.params.email, req.params.password)
+    .then((user) => {
+      console.log(user);
+      res.send(user);
+    });
 });
 
 app.get("/account/all", function (req, res) {
-  res.send({
-    name: "peter",
-    email: "peter@mit.edu",
-    password: "secret",
+  dal.all().then((docs) => {
+    console.log(docs);
+    res.send(docs);
   });
 });
 
