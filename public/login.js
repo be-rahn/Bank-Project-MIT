222a35fa49458e5156bc84adf1168c2249be2224
Brooke Rahn <brooke.rahn@gmail.com>
2022-06-15 00:06:46 -0700

    update login.js

--- public/login.js
+++ public/login.js
@@ -1,145 +1,113 @@
 function Login() {
-    const [show, setShow] = React.useState(true);
-    const [status, setStatus] = React.useState("");
-    const [name, setName] = React.useState("");
-    const [emailValue, setEmailValue] = React.useState("");
-    const [currentAccount, setCurrentAccount] = React.useState("");
-    const [emailVal, setEmailVal] = React.useState("");
-    const [passwordVal, setPasswordVal] = React.useState("");
-    const [buttonStatus, setButtonStatus] = React.useState(true);
-    const ctx = React.useContext(UserContext);
-  
-    if (show) {
-      for (const { name, email, loggedin } of ctx.users) {
-        if (loggedin) {
-          setShow(false);
-          setEmailValue(email);
-          setName(name);
-  
-          return;
-        }
-      }
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const ctx = React.useContext(UserContext);
+  const isEnabled = email.length > 0 || password.length > 8;
+
+  function validate(field, label) {
+    if (!field) {
+      setStatus("Error: " + label);
+      setTimeout(() => setStatus(""), 3000);
+      return false;
+    } else if (password.length < 8) {
+      setStatus("Error: " + "Password 8 characters");
+      setTimeout(() => setStatus(""), 3000);
+      return false;
     }
-  
-    function handleChange(e) {
-      if (e.target.value === null) {
-        setButtonStatus(true);
-        setPasswordVal(e.currentTarget.value);
-      } else {
-        setButtonStatus(false);
-        setPasswordVal(e.currentTarget.value);
-      }
-    }
-  
-    function validate(field, label) {
-      if (!field) {
-        setStatus("Error: Missing " + label);
-        setTimeout(() => setStatus(""), 3000);
-        return false;
-      }
-      return true;
-    }
-  
-    function handleLogin() {
-      if (!validate(emailVal, "email")) return;
-      if (!validate(passwordVal, "password")) return;
-      let tracker = false;
-  
-      for (const { email, password } of ctx.users) {
-        if (emailVal === email && passwordVal === password) {
-          for (var i = 0, length = ctx.users.length; i < length; i++) {
-            if (ctx.users[i].email === email) {
-              ctx.users[i].loggedin = true;
-              tracker = true;
-            }
-          }
-        }
-      }
-  
-      if (tracker) {
-        setShow(false);
-        setCurrentAccount(emailVal);
-      } else {
-        setStatus("Invalid login. Please create an account");
-        setTimeout(() => setStatus(""), 6000);
-      }
-    }
-  
-    function logout() {
-      for (var i = 0, length = ctx.users.length; i < length; i++) {
-        ctx.users[i].loggedin = false;
-      }
-      setShow(true);
-    }
-  
-    return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Card
-          bgcolor="primary"
-          txtcolor="white"
-          header="Login"
-          status={status}
-          body={
-            show ? (
-              <>
-                <br />
-                Email
-                <br />
-                <input
-                  type="input"
-                  className="form-control"
-                  id="email"
-                  placeholder="Enter Email"
-                  value={emailVal}
-                  onChange={(e) => setEmailVal(e.currentTarget.value)}
-                />
-                <br />
-                Password
-                <br />
-                <input
-                  type="password"
-                  className="form-control"
-                  id="password"
-                  placeholder="Enter Password"
-                  value={passwordVal}
-                  onChange={handleChange}
-                />
-                <br />
-                <button
-                  type="submit"
-                  className="btn btn-light"
-                  disabled={buttonStatus}
-                  onClick={handleLogin}
-                >
-                  Login
-                </button>
-                <br />
-                <br />
-              </>
-            ) : (
-              <>
-                <h5> {`Welcome back ${currentAccount}! `} </h5>
-                <br />
-                Check your account information{" "}
-                <a href="#/alldata" className="text-light">
-                  here
-                </a>
-                !
-                <br />
-                <br />
-                <button type="submit" className="btn btn-light" onClick={logout}>
-                  Logout
-                </button>
-              </>
-            )
-          }
-        />
-      </div>
-    );
+    return true;
   }
+
+  function handleLogin() {
+    console.log(email, password);
+    const url = `/account/login/${email}/${password}`;
+    (async () => {
+      var res = await fetch(url, { method: "get" });
+      var data = await res.json();
+      if (data.error) {
+        // failed to login
+        console.log(data.error);
+        setStatus("Error: " + data.error);
+      } else {
+        let user = data;
+        console.log(user);
+        ctx.users[0].name = user.name;
+        ctx.users[0].email = user.email;
+        ctx.users[0].password = user.password;
+        ctx.users[0].balance = user.balance;
+        setShow(false);
+      }
+    })();
+  }
+
+  function clearForm() {
+    setEmail("");
+    setPassword("");
+    setShow(true);
+  }
+
+  return (
+    <Card
+      bgcolor="primary"
+      header="Login"
+      status={status}
+      body={
+        show ? (
+          <>
+            Email address
+            <br />
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter email"
+              value={email}
+              pattern=".+@gmail\.com"
+              size="30"
+              required
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+            <br />
+            Password
+            <br />
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter password"
+              value={password}
+              minlength="8"
+              maxlength="20"
+              required
+              onChange={(e) => setPassword(e.currentTarget.value)}
+            />
+            <br />
+            <button
+              type="submit"
+              disabled={!isEnabled}
+              aria-disabled="true"
+              className="btn btn-light"
+              onClick={handleLogin}
+            >
+              Submit
+            </button>
+          </>
+        ) : (
+          <>
+            <div class="alert alert-success" role="alert">
+              <h4 class="alert-heading"></h4>
+              <p>Success!</p>
+
+              <button
+                type="submit"
+                className="btn btn-outline-success btn-sm"
+                onClick={clearForm}
+              ></button>
+            </div>
+          </>
+        )
+      }
+    />
+  );
+}
