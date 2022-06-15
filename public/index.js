340fe14e60225a4aaf2e4574725d43dca878f1b6
Brooke Rahn <brooke.rahn@gmail.com>
2022-06-15 00:05:31 -0700

    update index.js

--- public/index.js
+++ public/index.js
@@ -1,28 +1,29 @@
 function Spa() {
-    return (
-      <HashRouter>
-        <NavBar />
-        <UserContext.Provider
-          value={{
-            users: [
-              {
-                name: "Brooke Rahn",
-                email: "vjbrooklyn@yahoo.com",
-                password: "secret",
-                balance: "1000",
-              },
-            ],
-          }}
-        >
-          <Route path="/" exact component={Home} />
-          <Route path="/CreateAccount/" component={CreateAccount} />
-          <Route path="/login/" component={Login} />
-          <Route path="/deposit/" component={Deposit} />
-          <Route path="/withdraw/" component={Withdraw} />
-          <Route path="/alldata/" component={AllData} />
-        </UserContext.Provider>
-      </HashRouter>
-    );
-  }
-  
-  ReactDOM.render(<Spa />, document.getElementById("root"));
+  return (
+    <HashRouter>
+      <NavBar />
+      <UserContext.Provider
+        value={{
+          users: [
+            {
+              name: "",
+              email: "",
+              password: "",
+              balance: 0,
+            },
+          ],
+        }}
+      >
+        <Route path="/" exact component={Home} />
+        <Route path="/CreateAccount" component={CreateAccount} />
+        <Route path="/login" component={Login} />
+        <Route path="/deposit" component={Deposit} />
+        <Route path="/withdraw" component={Withdraw} />
+        <Route path="/balance" component={Balance} />
+        <Route path="/alldata" component={AllData} />
+      </UserContext.Provider>
+    </HashRouter>
+  );
+}
+
+ReactDOM.render(<Spa />, document.getElementById("root"));
